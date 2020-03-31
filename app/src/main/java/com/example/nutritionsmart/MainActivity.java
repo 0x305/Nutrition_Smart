cd918d851aa443ff64d547823ae515780907d05c
Adler <delra00000@gmail.com>
2020-03-31 14:03:32 -0400

    Added basic homepage

--- app/src/main/java/com/example/nutritionsmart/MainActivity.java
+++ app/src/main/java/com/example/nutritionsmart/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.nutritionsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Button Search;
 
 
     @Override
@@ -14,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*Search = (Button) findViewById(R.id.Search);
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSearch();
+            }
+        });*/
     }
+
+    /*public void openSearch() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }*/
 
 }
