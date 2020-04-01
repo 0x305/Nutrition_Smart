074c81cec6525e57757be900d4da42b0306d3c92
SeLahVi <feuerfaust125@gmail.com>
2020-04-01 01:00:57 -0500

    recent foods so far, with home tile

--- app/src/main/java/com/example/nutritionsmart/MainActivity.java
+++ app/src/main/java/com/example/nutritionsmart/MainActivity.java
@@ -9,14 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.RecentFoodsB);
-        button.setOnClickListener(new View.OnClickListener(){
+        button2 = findViewById(R.id.RecentFoodsB);
+        button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                moveToRecentFoods();
