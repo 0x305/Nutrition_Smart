ba858cec5f46325b145b38f74f72d408cc67f106
SeLahVi <feuerfaust125@gmail.com>
2020-03-31 22:46:23 -0500

    recent foods so far, with home tile

--- app/src/main/java/com/example/nutritionsmart/RecentFoods.java
+++ app/src/main/java/com/example/nutritionsmart/RecentFoods.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RecentFoods extends AppCompatActivity {
 
@@ -28,6 +30,25 @@ public class RecentFoods extends AppCompatActivity {
         setContentView(R.layout.activity_recent_foods);
 
         listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter( this, mTitle,mDescription, images);
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                if(position ==0){
+                    Toast.makeText(RecentFoods.this,"A hamburger",Toast.LENGTH_SHORT).show();
+                }
+                if(position ==0){
+                    Toast.makeText(RecentFoods.this,"Also a hamburger",Toast.LENGTH_SHORT).show();
+                }
+                if(position ==0){
+                    Toast.makeText(RecentFoods.this,"Yet another hamburger",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     class MyAdapter extends ArrayAdapter<String> {
@@ -49,7 +70,7 @@ public class RecentFoods extends AppCompatActivity {
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.row,parent, attachToRoot false);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
             ImageView images = row.findViewById(R.id.image);
             TextView myTitle = row.findViewById(R.id.textView1);
             TextView myDescription = row.findViewById(R.id.textView2);
@@ -58,7 +79,7 @@ public class RecentFoods extends AppCompatActivity {
             myTitle.setText(rTitle[position]);
             myDescription.setText(rDescription[position]);
 
-            return super.getView(position, convertView, parent);
+            return row;
         }
     }
 }
