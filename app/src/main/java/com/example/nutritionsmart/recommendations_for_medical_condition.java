8417b3cf7f857dffd6848e53c4e3144d702f3ead
Adler <delra00000@gmail.com>
2020-04-01 01:45:37 -0400

    added nilda's two screens

---
+++ app/src/main/java/com/example/nutritionsmart/recommendations_for_medical_condition.java
@@ -0,0 +1,14 @@
+package com.example.nutritionsmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recommendations_for_medical_condition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendations_for_medical_condition);
+    }
+}
